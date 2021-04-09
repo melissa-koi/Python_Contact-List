072ea32cf4dd32108583c76d387cdc6e11a03b96
melissa-koi <melissawangui3@gmail.com>
2021-04-09 23:03:05 +0300

    2nd commit

--- contact.py
+++ contact.py
@@ -1,2 +1,11 @@
 class Contact:
-    pass
+
+    contact_list = []
+
+    def __init__ (self, first_name, last_name, phone_number, email) :
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
+    
+
