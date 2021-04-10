f2fc53b8ab1fafe8ce26a32e186e825e7a4e40c5
melissa-koi <melissawangui3@gmail.com>
2021-04-10 12:15:44 +0300

    5th commit

--- contact.py
+++ contact.py
@@ -2,7 +2,7 @@ class Contact:
 
     contact_list = []
 
-    def __init__ (self, first_name, last_name, phone_number, email) :
+    def __init__(self, first_name, last_name, phone_number, email):
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
--- contact_test.py
+++ contact_test.py
@@ -5,10 +5,10 @@ from contact import Contact
 class TestContact(unittest.TestCase):
 
     def setUp(self):
-        self.newContact = Contact("James", "Muriuki", "0712345678", "james@gmail.com")
+        self.new_contact = Contact("James", "Muriuki", "0712345678", "james@gmail.com")
 
     def test_init(self):
-        self.assertEqual(self.newContact.first_name, "James")
+        self.assertEqual(self.new_contact.first_name, "James")
         self.assertEqual(self.new_contact.last_name, "Muriuki")
         self.assertEqual(self.new_contact.phone_number, "0712345678")
         self.assertEqual(self.new_contact.email, "james@gmail.com")
@@ -16,3 +16,14 @@ class TestContact(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+
+    def test_save_contact(self):
+        self.new_contact.save_contact()
+        self.assertEqual(len(Contact.contact_list), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
